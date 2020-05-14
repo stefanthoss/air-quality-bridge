888dade64d7c2a021831782fce02c3909910d8ab
Stefan Thoss <stef@nthoss.de>
2020-05-13 19:51:49 -0700

    First working prototype

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# luftdaten-influxdb-bridge
+# Luftdaten InfluxDB Bridge
+
+This Flask-based server accepts particulate matter/temperature/humidity data from a [luftdaten.info sensor](https://luftdaten.info/en/construction-manual) and forwards it to a InfluxDB instance.
+
+The luftdaten.info sensor already supports writing to InfluxDB directly but that requires you to expose the InfluxDB instance to the open web if it is located in a different network.
+
+## Requirements
+
+This project uses Python 3. Install required dependencies with
+
+```shell
+pip install Flask Flask-InfluxDB
+```
+
+## Configuration
+
+Change `INFLUXDB_HOST` in the `app.cfg` file to point to your InfluxDB.
+
+## Development
+
+Launch the app:
+
+```shell
+FLASK_APP=app.py FLASK_ENV=development flask run --host=0.0.0.0 --cert=adhoc --port 5443
+```
---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, jsonify, request
+from flask_influxdb import InfluxDB
+
+app = Flask(__name__)
+app.config.from_pyfile("app.cfg")
+influx_db = InfluxDB(app=app)
+
+
+def transform_data(data):
+    data_points = {}
+    for dp in data:
+        data_points[dp["value_type"]] = float(dp["value"])
+    return data_points
+
+
+@app.route("/", methods=["GET"])
+def root():
+    return jsonify({"name": app.name})
+
+
+@app.route("/post", methods=["POST"])
+def post():
+    data = request.json
+    app.logger.debug(f"Received data: {data}")
+    data_points = transform_data(data["sensordatavalues"])
+    app.logger.debug(f"Writing data: {data_points}")
+    influx_db.write_points(
+        [
+            {
+                "fields": data_points,
+                "tags": {"node": f"esp8266-{data['esp8266id']}"},
+                "measurement": "feinstaub",
+            }
+        ]
+    )
+
+    return jsonify({"success": "true"})
+
+
+if __name__ == "__main__":
+    app.run()
