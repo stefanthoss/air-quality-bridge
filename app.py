cfd61d54997e79459c17b880f55a53917c7b8ad3
Stefan Thoss <stefanthoss@users.noreply.github.com>
2020-08-24 13:43:56 -0700

    Support RPi node tag

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 app.config.from_pyfile("app.cfg")
 influx_db = InfluxDB(app=app)
 
+MEASUREMENT_NAME = "feinstaub"
 
 def transform_data(data):
     data_points = {}
@@ -23,9 +24,16 @@ def post():
     data = request.json
     app.logger.debug(f"Received data: {data}")
     data_points = transform_data(data["sensordatavalues"])
+    
+    node_tag = "unknown"
+    if "esp8266id" in data:
+        node_tag = f"esp8266-{data['esp8266id']}"
+    elif "rpiid" in data:
+        node_tag = f"rpi-{data['rpiid']}"
+
     app.logger.debug(f"Writing data: {data_points}")
     influx_db.write_points(
-        [{"fields": data_points, "tags": {"node": f"esp8266-{data['esp8266id']}"}, "measurement": "feinstaub"}]
+        [{"fields": data_points, "tags": {"node": node_tag}, "measurement": MEASUREMENT_NAME}]
     )
 
     return jsonify({"success": "true"})
