bd520d8800b06ac6a00c0cda0101fe7f8e0858b3
Stefan Thoss <stefanthoss@users.noreply.github.com>
2020-08-23 21:42:41 -0700

    Calculate AQI

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
+#!/usr/bin/python3
+
 from flask import Flask, jsonify, request
 from flask_influxdb import InfluxDB
 import aqi
+
+AQI_CATEGORIES = {
+    (0, 50): "Good",
+    (50, 100): "Moderate",
+    (100, 150): "Unhealthy for Sensitive Groups",
+    (150, 200): "Unhealthy",
+    (200, 300): "Very Unhealthy",
+    (300, 500): "Hazardous",
+}
 
 app = Flask(__name__)
 app.config.from_pyfile("app.cfg")
@@ -14,6 +25,12 @@ def transform_data(data):
     return data_points
 
 
+def get_aqi_category(aqi_value):
+    for limits, category in AQI_CATEGORIES.items():
+        if aqi_value > limits[0] and aqi_value <= limits[1]:
+            return category
+
+
 @app.route("/", methods=["GET"])
 def root():
     return jsonify({"name": app.name})
@@ -24,6 +41,18 @@ def post():
     data = request.json
     app.logger.debug(f"Received data: {data}")
     data_points = transform_data(data["sensordatavalues"])
+
+    aqi_value = float(
+        aqi.to_aqi(
+            [
+                (aqi.POLLUTANT_PM25, data_points["SDS_P2"]),
+                (aqi.POLLUTANT_PM10, data_points["SDS_P1"]),
+            ]
+        )
+    )
+    data_points["AQI_value"] = aqi_value
+    data_points["AQI_category"] = get_aqi_category(aqi_value)
+
     app.logger.debug(f"Writing data: {data_points}")
     influx_db.write_points(
         [
