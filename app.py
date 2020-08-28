8034ed043a1e375449a87b1ccc80fa33bcef50f3
Stefan Thoss <stefanthoss@users.noreply.github.com>
2020-08-27 22:09:20 -0700

    Rename endpoint to upload_measurement

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ influx_db = InfluxDB(app=app)
 
 MEASUREMENT_NAME = "feinstaub"
 
+
 def transform_data(data):
     data_points = {}
     for dp in data:
@@ -19,12 +20,12 @@ def root():
     return jsonify({"name": app.name})
 
 
-@app.route("/post", methods=["POST"])
-def post():
+@app.route("/upload_measurement", methods=["POST"])
+def upload_measurement():
     data = request.json
     app.logger.debug(f"Received data: {data}")
     data_points = transform_data(data["sensordatavalues"])
-    
+
     node_tag = "unknown"
     if "esp8266id" in data:
         node_tag = f"esp8266-{data['esp8266id']}"
@@ -32,9 +33,7 @@ def post():
         node_tag = f"rpi-{data['rpiid']}"
 
     app.logger.debug(f"Writing data: {data_points}")
-    influx_db.write_points(
-        [{"fields": data_points, "tags": {"node": node_tag}, "measurement": MEASUREMENT_NAME}]
-    )
+    influx_db.write_points([{"fields": data_points, "tags": {"node": node_tag}, "measurement": MEASUREMENT_NAME}])
 
     return jsonify({"success": "true"})
 
