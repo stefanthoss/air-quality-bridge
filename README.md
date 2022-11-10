7ab1fb753f45d66e578d3447b2f149aaca325524
Stefan Thoss <stefanthoss@users.noreply.github.com>
2022-11-09 19:03:29 -0800

    Publish sensor data to MQTT (#15)
    * Add MQTT/Home Assistant integration

--- README.md
+++ README.md
@@ -1,41 +1,8 @@
 # Air Quality Bridge
 
-This Flask-based server accepts particulate matter/temperature/humidity data from a [sensor.community sensor](https://sensor.community/en/sensors/airrohr) and writes it to a InfluxDB 2.0 server. It also calculates the Air Quality Index (AQI) with [hrbonz/python-aqi](https://github.com/hrbonz/python-aqi).
+This Flask-based server accepts air quality/temperature/humidity data from a [sensor.community sensor](https://sensor.community/en/sensors/airrohr) and forwards it to an InfluxDB 2.0 server or a Home Assistant instance through an MQTT broker. It also calculates the Air Quality Index (AQI) with [hrbonz/python-aqi](https://github.com/hrbonz/python-aqi).
 
-## Requirements
-
-This project uses Python 3. Install the required dependencies with
-
-```shell
-pip install -r requirements.txt
-```
-
-## Configuration
-
-The InfluxDB connection is configured via the environment variables `INFLUXDB_V2_URL`, `INFLUXDB_V2_TOKEN`, and `INFLUXDB_V2_ORG`. Other configuration parameters for InfluxDB are documented in the [influxdb-client-python README](https://github.com/influxdata/influxdb-client-python#via-environment-properties).
-
-Use `INFLUXDB_BUCKET` to configure the bucket (default: `sensors`) and `INFLUXDB_MEASUREMENT` to configure the measurement name (default: `air_quality`).
-
-## Development
-
-Launch the app locally in development mode and access it at <http://localhost:5000>:
-
-```shell
-export INFLUXDB_V2_URL="https://localhost:8086"
-export INFLUXDB_V2_TOKEN="my-token"
-export INFLUXDB_V2_ORG="my-org"
-export INFLUXDB_BUCKET="my-bucket"
-export INFLUXDB_MEASUREMENT="air_quality"
-python main.py
-```
-
-If everything is configured correctly, executing `curl -X GET http://localhost:5000/info` should return a JSON object that indicates the InfluxDB client is ready.
-
-If you want to build the Docker image locally, execute:
-
-```shell
-docker build -t air-quality-bridge:devel .
-```
+The bridge supports receiving data from multiple air quality sensors at the same time and is configured using environment variables.
 
 ## Deployment
 
@@ -45,7 +12,61 @@ The best way to deploy the application is with Docker Compose. Download the `doc
 
 If your InfluxDB server doesn't use a trusted SSL certificate, you'll have to add the environment variable `INFLUXDB_V2_VERIFY_SSL=False`.
 
-## Sample Payload
+## InfluxDB Configuration
+
+Set `ENABLE_INFLUXDB=true` to enable writing data to InfluxDB. The InfluxDB connection is configured using the environment variables `INFLUXDB_V2_URL`, `INFLUXDB_V2_TOKEN`, and `INFLUXDB_V2_ORG`. Other configuration parameters for InfluxDB are documented in the [influxdb-client-python README](https://github.com/influxdata/influxdb-client-python#via-environment-properties). Use `INFLUXDB_BUCKET` to configure the bucket (default: `sensors`) and `INFLUXDB_MEASUREMENT` to configure the measurement name (default: `air_quality`).
+
+## Home Assistant / MQTT Integration
+
+Set `ENABLE_MQTT=true` to enable writing data to MQTT and exposing the data as a sensor in Home Assistant. The MQTT broker is configured using the environment variables `MQTT_BROKER_URL`, `MQTT_BROKER_PORT`, `MQTT_USERNAME`, `MQTT_PASSWORD`, and `MQTT_CLIENT_ID`. Other configuration parameters for MQTT are documented in the [flask-mqtt README](https://flask-mqtt.readthedocs.io/en/latest/configuration.html#configuration-keys).
+
+In Home Assistant, add the [MQTT integration](https://www.home-assistant.io/integrations/mqtt/) and enable *Enable newly added entities* in the integration's system options. Once the Air Quality Bridge is running and receives data, it will publish data to MQTT which Home Assistant will use to create devices and entities for the air quality sensor through MQTT's auto discovery feature.
+
+## Sensor Configuration
+
+In the *Configuration* / *APIs* section of your air quality sensor:
+
+* Enable *Send data to custom API*.
+* Set *Server* to the IP of your Docker deployment.
+* Set *Pathth* to `/upload_measurement`.
+* Set *Port* to 5000.
+
+## Development / Non-Docker Usage
+
+This project uses Python 3. Install the required dependencies with
+
+```shell
+pip install -r requirements.txt
+```
+
+Launch the app locally in development mode and access it at <http://localhost:5000>:
+
+```shell
+export ENABLE_INFLUXDB=true
+export INFLUXDB_V2_URL="https://localhost:8086"
+export INFLUXDB_V2_TOKEN="my-token"
+export INFLUXDB_V2_ORG="my-org"
+export INFLUXDB_BUCKET="my-bucket"
+export INFLUXDB_MEASUREMENT="air_quality"
+export ENABLE_INFLUXDB=true
+export MQTT_BROKER_URL="localhost"
+export MQTT_BROKER_PORT=1883
+export MQTT_USERNAME="my-username"
+export MQTT_PASSWORD="my-password"
+python main.py
+```
+Add the environment variable `FLASK_DEBUG=1` for further debugging.
+
+If everything is configured correctly, executing `curl -X GET http://localhost:5000/info` should return a JSON object that indicates the application is ready. This endpoint is also used for the Docker health check.
+
+If you want to build the Docker image locally, execute:
+
+```shell
+docker build -t air-quality-bridge:devel .
+```
+
+
+## Sample Sensor Payload
 
 From the sensor firmware version `NRZ-2020-129`:
 
@@ -94,11 +115,4 @@ From the sensor firmware version `NRZ-2020-129`:
 }
 ```
 
-## Sensor Configuration
-
-In the *Configuration* section of your air quality sensor:
-
-* Set *Server* to the IP of your deployment.
-* Activate *Send data to custom API* and *HTTPS*.
-* Set */path* to `/upload_measurement`.
-* Set *Port* to 5443.
+Use `curl -X POST -H "Content-Type: application/json" -d @test/measurement.json http://127.0.0.1:5000/upload_measurement` to use this test file locally.
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 
+import json
 import os
 
 import aqi
 from flask import Flask, jsonify, request
+from flask_mqtt import Mqtt
 from influxdb_client import InfluxDBClient
 from influxdb_client.client.write_api import SYNCHRONOUS
+from waitress import serve
 
 AQI_CATEGORIES = {
     (-1, 50): "Good",
@@ -16,13 +19,93 @@ AQI_CATEGORIES = {
     (300, 500): "Hazardous",
 }
 
-influxdb_bucket = os.environ.get("INFLUXDB_BUCKET", "sensors")
-influxdb_measurement = os.environ.get("INFLUXDB_MEASUREMENT", "air_quality")
-
 app = Flask(__name__)
 
-client = InfluxDBClient.from_env_properties()
-write_api = client.write_api(write_options=SYNCHRONOUS)
+ENABLE_INFLUXDB = os.environ.get("ENABLE_INFLUXDB", "false").lower() == "true"
+app.logger.debug(f"InfluxDB enabled: {ENABLE_INFLUXDB}")
+ENABLE_MQTT = os.environ.get("ENABLE_MQTT", "false").lower() == "true"
+app.logger.debug(f"MQTT enabled: {ENABLE_MQTT}")
+
+if not ENABLE_INFLUXDB and not ENABLE_MQTT:
+    app.logger.warning("No data destination configured, the bridge will not forward incoming data.")
+
+if ENABLE_INFLUXDB:
+    app.logger.info("Creating InfluxDB connection...")
+    influxdb_client = InfluxDBClient.from_env_properties()
+    write_api = influxdb_client.write_api(write_options=SYNCHRONOUS)
+    influxdb_bucket = os.environ.get("INFLUXDB_BUCKET", "sensors")
+    influxdb_measurement = os.environ.get("INFLUXDB_MEASUREMENT", "air_quality")
+
+if ENABLE_MQTT:
+    app.logger.info("Creating MQTT connection...")
+    app.config["MQTT_BROKER_URL"] = os.environ.get("MQTT_BROKER_URL")
+    app.config["MQTT_BROKER_PORT"] = int(os.environ.get("MQTT_BROKER_PORT", 1883))
+    app.config["MQTT_USERNAME"] = os.environ.get("MQTT_USERNAME")
+    app.config["MQTT_PASSWORD"] = os.environ.get("MQTT_PASSWORD")
+    app.config["MQTT_CLIENT_ID"] = os.environ.get("MQTT_CLIENT_ID", "air-quality-bridge")
+    mqtt = Mqtt(app)
+
+
+def register_mqtt_sensor(device_name, sensor_name, device_info_dict):
+    device_class = None  # https://developers.home-assistant.io/docs/core/entity/sensor/#available-device-classes
+    sensor_name_readable = sensor_name
+    unit_of_measurement = None
+    enabled_by_default = "true"
+
+    if sensor_name.endswith("P0"):
+        device_class = "pm1"
+        sensor_name_readable = "PM 1"
+        unit_of_measurement = "µg/m³"
+    elif sensor_name.endswith("P1"):
+        device_class = "pm10"
+        sensor_name_readable = "PM 10"
+        unit_of_measurement = "µg/m³"
+    elif sensor_name.endswith("P2"):
+        device_class = "pm25"
+        sensor_name_readable = "PM 2.5"
+        unit_of_measurement = "µg/m³"
+    elif sensor_name.endswith("temperature"):
+        device_class = "temperature"
+        sensor_name_readable = "Temperature"
+        unit_of_measurement = "°C"
+    elif sensor_name.endswith("humidity"):
+        device_class = "humidity"
+        sensor_name_readable = "Humidity"
+        unit_of_measurement = "%"
+    elif sensor_name.endswith("pressure"):
+        device_class = "pressure"
+        sensor_name_readable = "Pressure"
+        unit_of_measurement = "Pa"
+    elif sensor_name.endswith("lux"):
+        device_class = "illuminance"
+        sensor_name_readable = "Light"
+        unit_of_measurement = "lx"
+    elif sensor_name == "AQI_value":
+        device_class = "aqi"
+        sensor_name_readable = "AQI"
+    elif sensor_name == "AQI_category":
+        sensor_name_readable = "AQI Category"
+    else:
+        enabled_by_default = "false"
+
+    ha_sensor_config = {
+        "availability_topic": f"homeassistant/sensor/{device_name}/status",
+        "device": device_info_dict,
+        "enabled_by_default": enabled_by_default,
+        "name": f"{device_name} {sensor_name_readable}",
+        "state_class": "measurement",
+        "state_topic": f"homeassistant/sensor/{device_name}/state",
+        "unique_id": f"{device_name}_{sensor_name}",
+        "value_template": f"{{{{ value_json.{sensor_name} }}}}",
+    }
+    if device_class is not None:
+        ha_sensor_config["device_class"] = device_class
+    if unit_of_measurement is not None:
+        ha_sensor_config["unit_of_measurement"] = unit_of_measurement
+
+    # Publish configuration
+    app.logger.debug(f"Configuring MQTT sensor: {ha_sensor_config}")
+    mqtt.publish(f"homeassistant/sensor/{device_name}/{sensor_name}/config", json.dumps(ha_sensor_config))
 
 
 def transform_data(data):
@@ -39,14 +122,17 @@ def get_aqi_category(aqi_value):
 
 
 @app.route("/info", methods=["GET"])
-def root():
-    return jsonify({"app_name": app.name, "influxdb_client": client.ready().status})
+def info():
+    response = {"app_name": app.name}
+    if ENABLE_INFLUXDB:
+        response["influxdb_client"] = influxdb_client.ready().status
+    return jsonify(response)
 
 
 @app.route("/upload_measurement", methods=["POST"])
 def upload_measurement():
     data = request.json
-    app.logger.debug(f"Received data: {data}")
+    app.logger.debug(f"Received data from sensor: {data}")
     data_points = transform_data(data["sensordatavalues"])
 
     node_tag = "unknown"
@@ -57,28 +143,50 @@ def upload_measurement():
 
     aqi_value = None
     if "SDS_P1" in data_points and "SDS_P2" in data_points:
-        aqi_value = int(
+        aqi_value = float(
             aqi.to_aqi([(aqi.POLLUTANT_PM10, data_points["SDS_P1"]), (aqi.POLLUTANT_PM25, data_points["SDS_P2"])])
         )
     elif "PMS_P1" in data_points and "PMS_P2" in data_points:
-        aqi_value = int(
+        aqi_value = float(
             aqi.to_aqi([(aqi.POLLUTANT_PM10, data_points["PMS_P1"]), (aqi.POLLUTANT_PM25, data_points["PMS_P2"])])
         )
     else:
-        app.logger.warn("Measurement for {node_tag} does not contain pollutant data.")
+        app.logger.warn("Measurement for {node_tag} does not contain air pollutant data.")
 
     if aqi_value is not None:
         data_points["AQI_value"] = aqi_value
         data_points["AQI_category"] = get_aqi_category(aqi_value)
 
-    app.logger.debug(f"Writing data: {data_points}")
-    write_api.write(
-        bucket=influxdb_bucket,
-        record=[{"measurement": influxdb_measurement, "tags": {"node": node_tag}, "fields": data_points}],
-    )
+    app.logger.debug(f"Parsed and transformed data: {data_points}")
+
+    if ENABLE_INFLUXDB:
+        app.logger.debug("Writing data to InfluxDB...")
+        write_api.write(
+            bucket=influxdb_bucket,
+            record=[{"measurement": influxdb_measurement, "tags": {"node": node_tag}, "fields": data_points}],
+        )
+
+    if ENABLE_MQTT:
+        app.logger.debug("Writing data to MQTT...")
+
+        ip_addr = request.environ.get("HTTP_X_FORWARDED_FOR", request.remote_addr)
+        device_info_dict = {
+            "configuration_url": f"http://{ip_addr}",
+            "identifiers": node_tag,
+            "manufacturer": "Sensor.Community",
+            "name": f"Air Sensor {node_tag}",
+            "sw_version": data["software_version"],
+            "via_device": "air-quality-bridge",
+        }
+
+        # Publish HA sensor data to MQTT
+        for sensor_name in data_points:
+            register_mqtt_sensor(node_tag, sensor_name, device_info_dict)
+        mqtt.publish(f"homeassistant/sensor/{node_tag}/status", "online")
+        mqtt.publish(f"homeassistant/sensor/{node_tag}/state", json.dumps(data_points))
 
     return jsonify({"success": "true"})
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    serve(app, host="0.0.0.0", port=5000)
